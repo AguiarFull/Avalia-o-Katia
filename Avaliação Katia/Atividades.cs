85094f0f510ed463c62eac8b45391ff58e7f1e37
Aluno <Aluno@LAB06-24>
2022-10-10 21:02:22 -0300

    Adicionar arquivos de projeto.

---
+++ Avaliação Katia/Atividades.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avaliação_Katia
+{
+    internal class Atividades
+    {
+
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Local { get; set; }
+        public string Data { get; set; }
+        public string Hora { get; set; }
+        
+        public (string Codigo, string Nome, string Local, string Data, string Hora);
+            {
+             
+            
+    }
+    
+        
+    
+
+  
+        
+      
+}
